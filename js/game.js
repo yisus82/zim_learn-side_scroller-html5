0ebc9885a0462e2359c79c16fc4a5f33c57ae864
yisus82 <djalma8@gmail.com>
2024-11-25 09:54:38 +0100

    Add colors and bounciness to the olive

--- js/game.js
+++ js/game.js
@@ -1,7 +1,21 @@
 const ready = () => {
-  const physics = new Physics().drag();
+  new Physics().drag();
 
-  const olive = new Circle().center().addPhysics();
+  const olive = new Circle({
+    radius: 60,
+    color: new RadialColor([green.darken(0.3), 'olive']),
+  })
+    .center()
+    .addPhysics({
+      bounciness: 0.5,
+    });
+  new Circle({
+    radius: 20,
+    color: black,
+  })
+    .alp(0.6)
+    .center(olive)
+    .mov(25);
 };
 
 new Frame({
